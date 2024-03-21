94366a9cf81614abc30361362826d1dee3aabb90
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-03-21 22:36:01 +0630

    update(): db connection setup finished

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"log"
 
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	const postgresURL = "postgres://admin:admin@localhost:5432/sqlc?sslmode=disable"
+	db, err := sql.Open("postgres", postgresURL)
+	defer db.Close()
+	if err != nil {
+		log.Println("Something went wrong while connecting to database!!")
+		panic(err)
+	}
 }
